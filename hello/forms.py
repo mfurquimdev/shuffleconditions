21be28cd1ddca04768e5f20ba203d6e5e5c72339
Mateus Furquim <furquim.axs@gmail.com>
2017-04-28 17:28:33 -0300

    Testing deploy

--- hello/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-CHOICES=[('1','1'), ('2','2'), ('3','3'), ('4','4'), ('5','5'), ('6','6'), ('7','7'), ('8','8'), ('9','9')] 
-
-class NumberForm(forms.Form):
-    numberchoices = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect())
